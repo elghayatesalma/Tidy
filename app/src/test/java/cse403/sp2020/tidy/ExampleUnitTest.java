38ddd1fcf61e2c3b3ea35fa47743100b66535558
Daniel Starikov <starikov@uw.edu>
2020-05-05 16:11:00 -0700

    removing failing unit test case to test CI

--- app/src/test/java/cse403/sp2020/tidy/ExampleUnitTest.java
+++ app/src/test/java/cse403/sp2020/tidy/ExampleUnitTest.java
@@ -14,9 +14,4 @@ public class ExampleUnitTest {
   public void addition_isCorrect() {
     assertEquals(4, 2 + 2);
   }
-
-  @Test
-  public void addition_isIncorrect() {
-    assertEquals(4, 1 + 1);
-  }
 }
