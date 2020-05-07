7ef059802053a9bdf3494a55a761aed4fe3f1c8a
klandon2-1736208 <landonkcee@gmail.com>
2020-05-07 16:30:50 -0700

    new frontend branch test

---
+++ app/src/main/java/cse403/sp2020/tidy/ui/ProfileActivity.java
@@ -0,0 +1,17 @@
+package cse403.sp2020.tidy.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cse403.sp2020.tidy.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
