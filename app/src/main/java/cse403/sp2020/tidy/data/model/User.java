5ddf8986648c902c9775f8c60866860040c4992d
John McMahon <mcmahjoh@uw.edu>
2020-05-07 20:47:27 -0700

    Model classes

--- app/src/main/java/cse403/sp2020/tidy/data/model/User.java
+++ app/src/main/java/cse403/sp2020/tidy/data/model/User.java
@@ -5,7 +5,37 @@ import java.util.ArrayList;
 
 public class User {
     String userId;
-    String UserName;
+    String userName;
     ArrayList<Task> preferences;
 
+    // TODO: Need to figure out how to import FirebaseUser in order to construct these.
+//    public User(FirebaseUser user, ArrayList<Task> preferences) {
+//        this.userId = user.getUid;
+//        this.username = user.getDisplayName();
+//        this.preferences = preferences;
+//    }
+
+    public ArrayList<Task> getPreferences() {
+        return preferences;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setPreferences(ArrayList<Task> preferences) {
+        this.preferences = preferences;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
