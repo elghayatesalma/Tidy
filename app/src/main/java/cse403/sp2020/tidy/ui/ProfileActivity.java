2e8769d1e2f4a521fb1806ee932404ee6ce71002
klandon2-1736208 <landonkcee@gmail.com>
2020-05-10 11:59:13 -0700

    added new profile activity and example user

--- app/src/main/java/cse403/sp2020/tidy/ui/ProfileActivity.java
+++ app/src/main/java/cse403/sp2020/tidy/ui/ProfileActivity.java
@@ -1,17 +1,88 @@
 package cse403.sp2020.tidy.ui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import cse403.sp2020.tidy.R;
+import cse403.sp2020.tidy.ui.profile.RecyclerAdapter;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    List<String> exampleChores = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
+    private LinearLayoutManager mLayoutManager;
+
+
+
+    // TODO: iterate through list of chores and add each of them to the display,
+    // TODO:
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // initialize example chores for testing
+        exampleChores.add("wash dishes");
+        exampleChores.add("take out garbage");
+        exampleChores.add("clean bathroom");
+        exampleChores.add("vacuum");
+
+        // this section makes the profile picture circular
+        ImageView profilePic = (ImageView) findViewById(R.id.profile_picture);
+        // might be able to replace R.drawable.example_user with user's selected image later on in the project
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.example_user);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        profilePic.setImageDrawable(roundedBitmapDrawable);
+
+        recyclerView = findViewById(R.id.chore_preference_list);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerAdapter = new RecyclerAdapter(exampleChores);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        RecyclerView.ItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(divider);
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder dragged, @NonNull RecyclerView.ViewHolder target) {
+                int draggedPosition = dragged.getAdapterPosition();
+
+                int targetPosition = target.getAdapterPosition();
+
+                Collections.swap(exampleChores, draggedPosition, targetPosition);
+
+                recyclerAdapter.notifyItemMoved(draggedPosition, targetPosition);
+
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            }
+        });
+
+        helper.attachToRecyclerView(recyclerView);
     }
+
+
 }
---
+++ app/src/main/java/cse403/sp2020/tidy/ui/profile/RecyclerAdapter.java
@@ -0,0 +1,60 @@
+package cse403.sp2020.tidy.ui.profile;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cse403.sp2020.tidy.R;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerAdapter";
+    private List<String> chorePreferences;
+
+    public RecyclerAdapter(List<String> chorePreferences) {
+        this.chorePreferences = chorePreferences;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //holder.rowCountTextView.setText(String.valueOf(position));
+        holder.textView.setText(chorePreferences.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chorePreferences.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView textView;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            textView = (TextView) v.findViewById(R.id.chore_preference_name);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), chorePreferences.get(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
