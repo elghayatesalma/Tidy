d368de26c996483fcb9936e7594386752f20b960
Daniel Starikov <starikov@uw.edu>
2020-05-30 15:01:08 -0700

    reassigning once a day

--- cloud-functions/firebase/main.py
+++ cloud-functions/firebase/main.py
@@ -1,6 +1,8 @@
 """ Google Cloud Function for Firestore Chore Assignments """
 from google.cloud import firestore
 import logging
+import random
+import datetime
 
 # Create a firestore client instance
 client = firestore.Client()
@@ -20,42 +22,101 @@ def update_assignments(data, context):
 
     @firestore.transactional
     def update_assignments_in_transaction(transaction, taskCollection, userCollection):
+        now = datetime.datetime.now()
+
         # Retrieve all the tasks in this household
         tasks = []
-        for taskDoc in taskCollection.stream(transaction):
-            tasks.append(taskDoc.to_dict())
+        taskIds = []
+        for task_doc in taskCollection.stream(transaction):
+            task_dict = task_doc.to_dict()
+            taskIds.append(task_dict['taskId'])
+            tasks.append(task_dict)
 
-        # Build a preference map from users to their preferences
-        preference_map = {}
+        # Collect the users
+        users = []
+        num_assignments = {}
         for user in userCollection.stream(transaction):
-            userData = user.to_dict()
-            preference_map[userData['firebaseId']] = userData['preferences']
+            user_data = user.to_dict()
+            num_assignments[user_data['firebaseId']] = 0
+            users.append(user_data)
+
+        unassigned = []
+        num_completed = 0
+        # Go through tasks and look for unassigned tasks
+        for task in tasks:
+            if 'assignedDate' in task:
+                last_assigned = task['assignedDate']
+                # reassign if it's been more than a day
+                # TODO: use task frequency field instead of a single day
+                if (now - last_assigned).days >= 1:
+                    unassigned.append(task)
+                elif 'assignedTo' in task and \
+                        ('completed' not in task \
+                            or task['completed'] is False):
+                    uid = task['assignedTo']
+                    # This user is not in the household anymore
+                    if uid not in num_assignments:
+                        unassigned.append(task)
+                    else:
+                        num_assignments[uid] += 1
+                else:
+                    num_completed += 1
+            else: # Never assigned before
+                unassigned.append(task)
+
+        # reassign all tasks if all are completed
+        if num_completed == len(tasks):
+            unassigned = tasks
+
+        preference_map = {}
+        for user_data in users:
+            uid = user_data['firebaseId']
+            # Make a user preference map for only unassigned tasks
+            if 'preferences' in user_data:
+                prefs = user_data['preferences']
+
+                # Add any missing preferences randomly to the end of pref list
+                to_add = [p for p in taskIds if p not in prefs]
+                random.shuffle(to_add)
+                all_prefs = prefs.extend(to_add)
+
+                # Get unassigned preferences
+                preference_map[uid] = [p for p in all_prefs if p in unassigned]
+            else:
+                # Give them a random preference list
+                preference_map[uid] = random.sample(unassigned, len(unassigned))
+
 
         # Compute user assignments
-        for task in tasks:
+        num_tasks = len(unassigned)
+        for task in unassigned:
+            priorities = {}
             taskId = task['taskId']
-            min_priority = 1000000
-            selected_user = None
-            # Go through all the preferences of users
-            for user in preference_map.keys():
-                # Find the index of the task in their preference list
-                priority = preference_map[user].index(taskId)
-                # Keep the minimum index and use that as the assignment
-                if priority < min_priority:
-                    min_priority = priority
-                    selected_user = user
 
-            task['assigned'] = selected_user
+            # Go through all the preferences of users
+            for uid, prefs in preference_map.items():
+                n = (num_tasks - len(prefs)) + num_assignments[uid]
+                priorities[uid] = prefs.index(taskId) + (n**2)
+
+            # Find the minimum priority users
+            min_priority = min(priorities.values()) 
+            min_priority_users = [u for u in priorities.keys() \
+                                    if priorities[u] == min_priority]
+
+            # randomly pick a user if there are conflicts 
+            selected_user = random.choice(min_priority_users)
+            task['assignedTo'] = selected_user
             logging.info('assigning task ' + taskId + ' to user ' + selected_user + ' with preference ' + str(min_priority))
 
-        # Update the assignments in firestore
-        for task in tasks:
-            # get the document using the taskID
-            taskDoc = taskCollection.document(task['taskId'])
-            # merge in the new assigned field
-            transaction.update(taskDoc, { u'assigned': task['assigned'] })
+            taskDoc = taskCollection.document(taskId)
+            # merge in the new assigned field and assigned date
+            transaction.update(taskDoc,
+                               {
+                                   u'assignedTo': selected_user,
+                                   u'assignedDate': now,
+                                   u'completed': False
+                               })
 
-        logging.info('all tasks: ' + str(tasks))
 
     # Run the update assignments transaction
     transaction = client.transaction()
